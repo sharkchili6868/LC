062b79e9baf62f2cddc16b1b77ce6494d78f5937
sharkchili6868 <40523036+sharkchili6868@users.noreply.github.com>
2020-08-19 15:13:51 -0700

    add new problem

---
+++ P0227_Basic_Calculator.java
@@ -0,0 +1,29 @@
+class Solution {
+	public int calculate(String s) {
+        if (s == null || s.length() == 0) return 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        s += "+";
+
+        char op = '+';
+
+        for(int i = 0, n = 0; i < s.length(); i++) {
+        	char c = s.charAt(i);
+        	if (c >= '0' && c <= '9') {
+        		n = n * 10 + c - '0';
+        		continue;
+        	}
+        	if (c == ' ') continue;
+        	if (op == '+') stack.push(n);
+        	else if (op == '-') stack.push(-n);
+        	else if (op == '*') stack.push(stack.pop() * n);
+        	else if (op == '/') stack.push(stack.pop() / n);
+        	op = c;
+        	n = 0;
+        }
+
+        int result = 0;
+        while(!stack.isEmpty()) total += stack.pop();
+
+        return result;
+    }
+}
